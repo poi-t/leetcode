22af1f695be55bd223414f4ed5e3e7a4ade0e21d
poi-t <2273304740@qq.com>
2021-01-20 16:01:55 +0800

    加入了少量注释

--- 剑指offer/n个骰子的点数.cpp
+++ 剑指offer/n个骰子的点数.cpp
@@ -9,20 +9,21 @@ public:
 
         for(int i = 2; i <= n; ++i)
         {
-            for(int j = 6*i; j >= i; --j)
+            for(int j = 6*i; j >= i; --j)//计算i个骰子可能出现的点数 
             {
                 dp[j] = 0;
                 for(int point = 1; point <= 6; ++point)
                 {
-                    if(j - point < i-1) break;
+                    if(j - point < i-1) break;//点数不能低于上一轮能够得到的最低点数 
                     dp[j] += dp[j-point];
                 }
             }
         }
+        
         int mod = 1;
         for(int i = 0; i < n; ++i) mod *= 6;
         vector<double> ans;
-        for(int i = n; i <= 6*n; ++i)
+        for(int i = n; i <= 6*n; ++i)//最终可能出现的点数只有n到6n 
         {
             ans.push_back(dp[i]*1.0/mod);
         }
