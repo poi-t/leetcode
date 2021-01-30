a0c6950cc6c1731e3357f887c2b2100c9fddc6f3
poi-t <2273304740@qq.com>
2021-01-30 16:31:39 +0800

    爬楼梯

---
+++ LeetCode热题HOT100/爬楼梯.cpp
@@ -0,0 +1,14 @@
+//假设你正在爬楼梯。需要n阶你才能到达楼顶。每次你可以爬 1或2个台阶，你有多少种不同的方法可以爬到楼顶
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1);
+        dp[0] = dp[1] = 1;
+        for(int i = 2; i <=n; ++i)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+};
